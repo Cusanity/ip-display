22943b6395288d75cb77e0fdf6cf00838e27147f
Yanchao Wang <wyc935398521@gmail.com>
2024-01-21 17:26:36 -0600

    add externel

--- get_ip.php
+++ get_ip.php
@@ -1,4 +1,5 @@
 <?php
-$serverIpAddress = $_SERVER['SERVER_ADDR'];
-echo json_encode(['ip' => $serverIpAddress]);
+$localIpAddress = $_SERVER['SERVER_ADDR'];
+$externalIpAddress = file_get_contents('https://api64.ipify.org?format=json');
+echo json_encode(['localIp' => $localIpAddress, 'externalIp' => json_decode($externalIpAddress, true)['ip']]);
 ?>
